5c8e441c445c0476673614a6f55793487de2a0c2
Piyush Jain <piyushjain@duck.com>
2025-06-19 09:17:24 -0700

    WIP: Updates to match toolkit changes

--- jupyter_ai_tools/__init__.py
+++ jupyter_ai_tools/__init__.py
@@ -1,6 +1,5 @@
-from .extension import jupyter_server_extension_tools
-
-__all__ = ["jupyter_server_extension_tools"]
+from .extension import get_git_tools, get_notebook_tools
+from jupyter_server_ai_tools.models import Tool, Toolkit
 
 __version__ = "0.1.2"
 
@@ -11,3 +10,20 @@ def _jupyter_server_extension_points():
 
 def _load_jupyter_server_extension(serverapp):
     serverapp.log.info("✅ jupyter_ai_tools extension loaded.")
+
+async def _start_jupyter_server_extension(serverapp):
+    registry = serverapp.extension_manager.extensions.get(
+        "jupyter_server_ai_tools"
+    )
+    if registry:
+        registry.register_toolkit(
+            Toolkit(
+                name="notebook_toolkit", tools=get_notebook_tools()
+            )
+        )
+
+        registry.register_toolkit(
+            Toolkit(
+                name="git_toolkit", tools=get_git_tools()
+            )
+        )
