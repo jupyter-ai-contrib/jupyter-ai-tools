6acb033a5343bba74cda5e219b6832346cdefbd8
Piyush Jain <piyushjain@duck.com>
2025-08-19 19:07:12 -0700

    Exporting toolkits in root module

--- jupyter_ai_tools/__init__.py
+++ jupyter_ai_tools/__init__.py
@@ -1,4 +1,16 @@
+from .toolkits.code_execution import toolkit as codeexec_toolkit
+from .toolkits.file_system import toolkit as fs_toolkit
+from .toolkits.git import toolkit as git_toolkit
+from .toolkits.notebook import toolkit as notebook_toolkit
+
 __version__ = "0.2.1"
+
+__all__ = [
+    "fs_toolkit",
+    "codeexec_toolkit", 
+    "git_toolkit",
+    "notebook_toolkit",
+]
 
 
 def _jupyter_server_extension_points():
