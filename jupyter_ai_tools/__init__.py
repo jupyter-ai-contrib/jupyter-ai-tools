59ee8432c2527dc9b45050408c4aebba70a3b2db
David L. Qiu <david@qiu.dev>
2026-03-24 16:24:17 -0700

    remove git toolkit, relax pycrdt range

--- jupyter_ai_tools/__init__.py
+++ jupyter_ai_tools/__init__.py
@@ -1,6 +1,5 @@
 from .toolkits.code_execution import toolkit as exec_toolkit
 from .toolkits.file_system import toolkit as fs_toolkit
-from .toolkits.git import toolkit as git_toolkit
 from .toolkits.notebook import toolkit as nb_toolkit
 
 __version__ = "0.4.1"
@@ -8,7 +7,6 @@ __version__ = "0.4.1"
 __all__ = [
     "fs_toolkit",
     "exec_toolkit",
-    "git_toolkit",
     "nb_toolkit",
 ]
 
